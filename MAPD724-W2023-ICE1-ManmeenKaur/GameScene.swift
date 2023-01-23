48cc48305fc882ac13cc4026f7f656e94b7706ea
manmeenkaur <manmeen.kaur.17@gmail.com>
2023-01-22 23:38:23 -0500

    added second ocean to the Scene

--- MAPD724-W2023-ICE1-ManmeenKaur/GameScene.swift
+++ MAPD724-W2023-ICE1-ManmeenKaur/GameScene.swift
@@ -11,16 +11,22 @@ import GameplayKit
 class GameScene: SKScene
 {
     // instance variables
-    var ocean: Ocean?
+    var ocean1: Ocean?
+    var ocean2: Ocean?
     
     override func sceneDidLoad()
     {
         name = "GAME"
 
-        // add the ocean to the Scene
-        ocean = Ocean()
-        ocean?.Reset()
-        addChild(ocean!)
+        // add the first ocean to the Scene
+        ocean1 = Ocean()
+        ocean1?.Reset()
+        addChild(ocean1!)
+
+        // add the second ocean to the scene
+        ocean2 = Ocean()
+        ocean2?.position.y = -773
+        addChild(ocean2!)
     }
     
     func touchDown(atPoint pos : CGPoint)
@@ -61,6 +67,7 @@ class GameScene: SKScene
     
     override func update(_ currentTime: TimeInterval)
     {
-        ocean?.Update()
+        ocean1?.Update()
+        ocean2?.Update()
     }
 }
